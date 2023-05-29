2cb613be2aa11c7a6a4163f4c050d422b2c5bffc
Akarikev <princeelorm17@gmail.com>
2023-05-29 02:35:00 -0700

    credits

--- src/scenes/Footer/index.tsx
+++ src/scenes/Footer/index.tsx
@@ -2,7 +2,7 @@ import Logo from "@/assets/Logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-100 py-16">
+    <footer className="bg-primary-100 pt-16">
       <div className="mx-auto w-5/6 justify-center gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0 ">
           <img src={Logo} alt="Logo" />
@@ -27,6 +27,14 @@ const Footer = () => {
           </p>
           <p>(233)123-456-7689</p>
         </div>
+      </div>
+
+      <div className="mt-16 flex basis-1/4 justify-center md:mb-0">
+        Developed by {""}{" "}
+        <span className="cursor-pointer font-bold underline hover:animate-bounce">
+          {" "}
+          @Akarikev
+        </span>
       </div>
     </footer>
   );
